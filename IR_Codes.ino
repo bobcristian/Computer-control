aba1d1b406860049a15df6454de828c0413bd5ab
bobcristian <bobcristian96@yahoo.com>
2018-03-03 20:30:42 +0200

    Add files via upload

---
+++ IR_Codes.ino
@@ -0,0 +1,26 @@
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
+
+const int recievePin = 10;
+IRrecv irrecv(recievePin);
+decode_results results;
+
+void setup() 
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  pinMode(13, OUTPUT);
+  digitalWrite(13,LOW);
+}
+
+void loop() 
+{
+  if(irrecv.decode(&results))
+  {
+   Serial.println(results.value,HEX);
+   irrecv.resume(); 
+  }
+  delay(10);
+}
